69f240c9fe5befbb7e1208479300c0f252055783
Titilope Ajayi oluwaseyi <ajayititilope357@gmail.com>
2023-10-02 00:09:34 +0100

    auth screens

---
+++ app/src/main/java/com/panther/contentai/EmailAuthScreen.kt
@@ -0,0 +1,30 @@
+package com.panther.contentai
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.panther.contentai.databinding.FragmentEmailAuthScreenBinding
+
+
+class EmailAuthScreen : Fragment() {
+lateinit var emailBinding:FragmentEmailAuthScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        emailBinding = FragmentEmailAuthScreenBinding.inflate(layoutInflater)
+        val view = emailBinding.root
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_email_auth_screen, container, false)
+    }
+
+
+    }
